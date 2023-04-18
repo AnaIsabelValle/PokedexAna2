d4d74b66fa15ac74fd7d65e3fb461857f19b3abe
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-04-18 20:19:53 +0100

    feat:Clase Main; Creación menú principal

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
-public class Main {
+import com.Pokedex.Pokemon.Pokemon;
+import com.Pokedex.Pokemon.PokemonLegendario;
+import com.Pokedex.Utilidades.ListaPokemon;
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
+        public class Main {
+
+            public static void main(String[] args) {
+
+                Scanner teclado = new Scanner(System.in);
+
+                System.out.println(" --------------**OOOO** --------------");
+                System.out.println("Querido usuario, le damos la bienvenida a nuestro menú Pokedex: ");
+
+                /* Creamos las listas de pokemon avistados y capturados*/
+                ListaPokemon avistados = new ListaPokemon();
+                ListaPokemon capturados = new ListaPokemon();
+
+                // Mostrar el menú principal //
+                String opcion = "";
+                boolean salida = false;
+
+                while (salida != true) {
+                    System.out.println("a. Añadir un pokemon");
+                    System.out.println("b. Añadir avistamiento legendario");
+                    System.out.println("c. Pasar a capturados");
+                    System.out.println("d. Mostrar listas de avistados");
+                    System.out.println("e. Mostrar lista de capturados");
+                    System.out.println("f. Cerrar la pokedex");
+
+                    System.out.print("Escoja una opción: ");
+                    opcion = teclado.nextLine();
+
+                }
+            }
+        }
