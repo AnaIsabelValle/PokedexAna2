43a34d3754070e97f7abea95981234a0040c5f3d
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-04-18 19:50:50 +0100

    feat:Clase ListaPokemon; Creación método añadir Pokemon

--- src/com/Pokedex/Utilidades/ListaPokemon.java
+++ src/com/Pokedex/Utilidades/ListaPokemon.java
@@ -10,4 +10,14 @@ public class ListaPokemon {
         public ListaPokemon() {
                 this.pokemonArray = new Pokemon[1];
         }
+        public void añadirPokemon(Pokemon nuevoPokemon) {
+                Pokemon[] nuevoPokemonArray = new Pokemon[this.totalPokemon + 1];
+
+                for (int i = 0; i < this.pokemonArray.length; i++) {
+                        nuevoPokemonArray[i] = this.pokemonArray[i];
+                }
+                pokemonArray [totalPokemon] = nuevoPokemon;
+                totalPokemon ++;
+                System.out.println( nuevoPokemon.getNombre() + " ha sido añadido a la lista de Pokemon");
+        }
 }
