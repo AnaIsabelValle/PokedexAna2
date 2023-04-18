073ac0f57f403cfca56af7db8dcc48b5b0eeb6a1
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-04-18 19:26:03 +0100

    fix:Clase PokemonLwegendario: Modificaición;atributo y constructor

--- src/com/Pokedex/Pokemon/PokemonLegendario.java
+++ src/com/Pokedex/Pokemon/PokemonLegendario.java
@@ -1,11 +1,16 @@
 package com.Pokedex.Pokemon;
 
-public class PokemonLegendario extends Pokemon{
+import com.Pokedex.Pokemon.Pokemon;
+
+public class PokemonLegendario extends Pokemon {
 
     protected String localizacion;
+
     public PokemonLegendario(String nombre, String tipo,
-                             float altura, float peso, String descripcion, String localizacion) {
+                             float altura, float peso,
+                             String descripcion, String localizacion) {
         super(nombre, tipo, altura, peso, descripcion);
+
         this.localizacion = localizacion;
     }
 
