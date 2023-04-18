154989cbee85b9f2822b2666555634f1337c3413
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-04-18 18:59:02 +0100

    feat: Clase Pokemon;creados los atributos

---
+++ src/com/Pokedex/Pokemon/Pokemon.java
@@ -0,0 +1,10 @@
+package com.Pokedex.Pokemon;
+
+public class Pokemon {
+
+        protected String nombre;
+        protected String tipo;
+        protected float altura;
+        protected float peso;
+        protected String descripcion;
+}
