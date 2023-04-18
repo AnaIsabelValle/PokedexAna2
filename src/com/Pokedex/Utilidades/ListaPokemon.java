f80892e6ecf47e47f50279fa30877f868a8fa987
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-04-18 19:42:10 +0100

    feat: Clase ListaPokemon; Creación de los Atributos

--- src/com/Pokedex/Utilidades/ListaPokemon.java
+++ src/com/Pokedex/Utilidades/ListaPokemon.java
@@ -1,4 +1,9 @@
 package com.Pokedex.Utilidades;
 
+import com.Pokedex.Pokemon.Pokemon;
+
 public class ListaPokemon {
+
+        protected Pokemon[] pokemonArray;
+        protected int totalPokemon = 0;
 }
