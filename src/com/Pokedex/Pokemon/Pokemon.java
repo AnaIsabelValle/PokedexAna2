9c425a123ead40d72939eba75f67fa44c7aa3e30
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-04-18 19:01:33 +0100

    feat:Clase Pokemon; creados los constructores

--- src/com/Pokedex/Pokemon/Pokemon.java
+++ src/com/Pokedex/Pokemon/Pokemon.java
@@ -7,4 +7,17 @@ public class Pokemon {
         protected float altura;
         protected float peso;
         protected String descripcion;
+
+        public Pokemon(String nombre, String tipo, float altura, float peso, String descripcion) {
+                this.nombre = nombre;
+                this.tipo = tipo;
+                this.altura = altura;
+                this.peso = peso;
+                this.descripcion = descripcion;
+        }
+
+        public Pokemon(String nombre, String tipo) {
+                this.nombre = nombre;
+                this.tipo = tipo;
+        }
 }
