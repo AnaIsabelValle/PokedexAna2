cf344dd38fff856830089bf8e7c1649a11bf0e39
AnaIsabelValle <usr3048@salesianos-lacuesta.net>
2023-04-18 19:18:02 +0100

    feat:Clase PokemonLegendario; Creación Atributo localización y constructores

--- src/com/Pokedex/Pokemon/PokemonLegendario.java
+++ src/com/Pokedex/Pokemon/PokemonLegendario.java
@@ -1,4 +1,16 @@
 package com.Pokedex.Pokemon;
 
 public class PokemonLegendario extends Pokemon{
+
+    protected String localizacion;
+    public PokemonLegendario(String nombre, String tipo,
+                             float altura, float peso, String descripcion, String localizacion) {
+        super(nombre, tipo, altura, peso, descripcion);
+        this.localizacion = localizacion;
+    }
+
+    public PokemonLegendario(String nombre, String tipo, String localizacion) {
+        super(nombre, tipo);
+        this.localizacion = localizacion;
+    }
 }
